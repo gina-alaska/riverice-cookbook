8c5febd499224db8df2ffe9ce3de12d04a2a2209
Will Fisher <will@gina.alaska.edu>
2016-03-18 11:34:28 -0800

    fixing install path for lmod and use system for python

--- recipes/lmod.rb
+++ recipes/lmod.rb
@@ -3,6 +3,7 @@ package %w{lua-devel lua-posix lua-filesystem rsync}
 remote_file "#{Chef::Config[:file_cache_path]}/lmod-5.4.tar.bz2" do
 	source node['riverice']['lmod_src_url']
 	notifies :run, 'script[install_lmod]'
+  not_if { ::File.exists?('/opt/apps') }
 end
 
 
@@ -12,8 +13,8 @@ script "install_lmod" do
 	code <<-EOC
 	tar xvjf lmod-5.4.tar.bz2
 	cd Lmod-5.4
-	./configure --prefix=/opt/lmod --with-spiderCacheDir=/opt/lmod/moduleData/cacheDir \
-	  --with-updateSystemFn=/opt/lmod/moduleData/system.txt
+	./configure --prefix=/opt/apps --with-spiderCacheDir=/opt/apps/lmod/moduleData/cacheDir \
+	  --with-updateSystemFn=/opt/apps/lmod/moduleData/system.txt
 	make
 	make install
 	EOC
--- recipes/python.rb
+++ recipes/python.rb
@@ -1,13 +1,11 @@
 # include_recipe "python"
-python_runtime '2'
+python_runtime '2' do
+  provider :system
+end
 
 package %w{ blas-devel lapack-devel libjpeg libjpeg-devel hdf5-devel netcdf-devel netcdf42-gcc-hdf5-188-devel }
 
-python_package "pillow"
-python_package "numpy"
-python_package "scipy" do
-	timeout 3600
-end
-
 ENV["HDF5_DIR"] = "/usr/local/netcdf/gcc/hdf5-1.8.8/4.2"
-python_package "netCDF4"
+%w{ pillow numpy scipy netCDF4 }.each do |python_module|
+  python_package python_module
+end
