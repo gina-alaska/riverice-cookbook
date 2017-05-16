f5a146d35b9791b551dec5b1ccdfd440f8376daa
Will Fisher <will@gina.alaska.edu>
2017-05-16 12:28:46 -0800

    remove old iptables code

--- recipes/ldm.rb
+++ recipes/ldm.rb
@@ -4,23 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-# iptables_ng_rule '388-ldm-tcp' do
-#   name '388-ldm-tcp'
-#   chain 'INPUT'
-#   table 'filter'
-#   rule '-m state --state NEW -p tcp -s 137.229.19.0/24,10.19.16.0/23,10.0.19.0/24 --dport 388 -j ACCEPT'
-#   ip_version 4
-#   action :create
-# end
-# iptables_ng_rule '388-ldm-udp' do
-#   name '388-ldm-udp'
-#   chain 'INPUT'
-#   table 'filter'
-#   rule '-p udp -s 137.229.19.0/24,10.19.16.0/23,10.0.19.0/24 --dport 388 -j ACCEPT'
-#   ip_version 4
-#   action :create
-# end
-
 include_recipe 'gina_firewall::default'
 
 %w{ 137.229.19.0/24 10.19.16.0/24 10.0.19.0/24 }.each do |iprange|
